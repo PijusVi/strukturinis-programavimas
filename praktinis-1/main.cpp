987ed599f43fc83022d467715ebe470b6ec5399e
Pijus Vismantas <s067375@ad.viko.lt>
2026-09-17 11:47:39 +0300

    cpp koda uzdejau

---
+++ praktinis-1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string Vard, Pav, Grup, StudPav;
+    int Amz, Kurs;
+
+    cout << "Ivesk savo varda: ";
+    cin >> Vard;
+
+    cout << "Ivesk savo pavarde: ";
+    cin >> Pav;
+
+    cout << "Ivesk savo amziu: ";
+    cin >> Amz;
+
+    cout << "Ivesk savo grupe: ";
+    cin >> Grup;
+
+    cout << "Ivesk savo kursa: ";
+    cin >> Kurs;
+
+    cout << "Ivesk savo studiju programa: ";
+    cin >> StudPav;
+
+    cout << "Vardas: " << Vard << endl;
+    cout << "Pavarde: " << Pav << endl;
+    cout << "Grupe: " << Grup << endl;
+    cout << "Kursas: " << Kurs << endl;
+    cout << "Studiju programa: " << StudPav << endl;
+    cout << "Grupe: " <<Grup << endl;
+    cout << "Kursas: " << Kurs << endl;
+    cout << "Studiju programa: " << StudPav << endl;
+
+    return 0;
+
+}
